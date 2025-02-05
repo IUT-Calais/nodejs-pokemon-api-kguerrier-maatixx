34bef92316ab657ed349d4a79ab0f4933cdd4ac2
Mathéïs Fardel <matheis.fardel@orange.fr>
2025-02-05 09:51:29 +0100

    model PokemonCard, seed.ts à jour et routes sur index.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -27,7 +27,19 @@ async function main() {
     ],
   });
 
-  console.log('Seed completed!');
+  await prisma.pokemonCard.deleteMany();
+    await prisma.pokemonCard.createMany({
+        data : [
+          { name: 'Pikachu', pokedexId: 25, typeId: 5, lifePoints: 35, weight: 6, size: 0.4, imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png' },
+          { name: 'Bulbizarre', pokedexId: 1, typeId: 4, lifePoints: 45, weight: 6.9, size: 0.7,  imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png' },
+          { name: 'Salamèche', pokedexId: 4, typeId: 2, lifePoints: 39, weight: 8.5, size: 0.6, imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png' },
+          { name: 'Carapuche', pokedexId: 7, typeId: 3, lifePoints: 44, weight: 9, size: 0.5, imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png' },
+          { name: 'Rondoudou', pokedexId: 39, typeId: 1, lifePoints: 115, weight: 5.5, size: 0.5, imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/039.png' },
+          { name: 'Miaouss', pokedexId: 52, typeId: 1, lifePoints: 40, weight: 4.2, size: 0.4, imageUrl: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/052.png' },
+        ]
+    });
+
+   console.log('Seed completed!');
 }
 
 main()
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
-
+import {Request, Response} from 'express';
+import prisma from "./client";
 export const app = express();
 const port = process.env.PORT || 3000;
 
@@ -10,3 +11,24 @@ export const server = app.listen(port);
 export function stopServer() {
   server.close();
 }
+
+app.get('/pokemon-cards', (req: Request, res: Response) => {
+  res.status(200).send("Liste de tous les Pokemons");
+});
+
+app.get('/pokemon-cards/:pokemonCardId', (_req: Request, res: Response) => {
+  res.status(200).send(`Affichage pokemon ${_req.params.pokemonCardId}`);
+});
+
+app.post('/pokemon-cards', (req: Request, res: Response) => {
+  res.status(201).send(`Pokemon ${req.body.name} créé`);
+});
+
+app.patch('/pokemon-cards/:pokemonCardId', (_req: Request, res: Response) => {
+  res.status(200).send(`Pokemon ${_req.params.pokemonCardId} modifié`);
+});
+
+app.delete('/pokemon-cards/:pokemonCardId', (_req: Request, res: Response) => {
+  res.status(200).send(`Pokemon ${_req.params.pokemonCardId} supprimé`);
+});
+
