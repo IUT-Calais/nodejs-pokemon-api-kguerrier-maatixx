a3f7a4eea42f38e8c14d993ef36eb4db790444ac
Mathéïs Fardel <157315624+maatixx@users.noreply.github.com>
2025-05-25 19:05:22 +0200

    index.ts pour le swagger

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import * as swaggerDocument from './swagger.json';
 import pokemonRoutes from "./pokemon/pokemon.router";
 import usersRoutes from "./users/users.router";
+
 export const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(pokemonRoutes);
 app.use(usersRoutes);
 
